c8f2d862b0b6a39320ef66cde5daccb5254d93f6
cnickc <camshaft75@hotmail.com>
2014-05-12 21:26:23 -0400

    minimal viable product.

--- CacheManager.php
+++ CacheManager.php
@@ -3,98 +3,92 @@
 	{
 		// property declaration
 		private $tmpCacheFile = 'tmp.cache';
-		private $folder = './cache/';
+		private $folder = '/cache/';
 		private $maxAge = 30;
 		
 		//constructors
-		function __construct(arr) {
+		function __construct($arr = array()) {
 			if(array_key_exists('timeToExpire', $arr))
 				$this->maxAge = $timeToExpire;	//the maximum lifetime of a cache file in seconds
 			if(array_key_exists('cachedir', $arr))
 				$this->tmpCacheFile = $tmpfile;	//the name of the tmp working file
 			if(array_key_exists('tmpfile', $arr))
-				$this->folder = $cachedir;		//the directory containing the cache files
+				$this->folder = $cachedir;		//the directory containing the cache files				
 		} 
 
 		// method declaration
 		public function retrieveCache() {
+		
 			$file = $this->retrieveCacheFile();
-			if(!empty($file) && !isExpired($file)) {
+			if(!empty($file) && !$this->isExpired($file)) {
 				return $file;		//This file is current. Return reference to it
 			}
 			
 			//otherwise, file is out of date or does not exist.  Attempt to create one
-			$fp = fopen($this->folder . $this->tmpCacheFile, "r+");
+			$fp = fopen(dirname(__FILE__) . $this->folder . $this->tmpCacheFile, "r+");
 
 			if (!flock($fp, LOCK_EX | LOCK_NB)) {  // acquire an exclusive lock
 				// Someone else is already updating the cache file. Nothing more to do here.
+				echo "file is already locked: " . time() . "<br />";
 				fclose($fp);			
 				return $file;	//return old file.  Will return null if no suitable file exists.
 			} 
+			
+			echo "exclusive lock acquired: " . time();
 
 			// Exclusive lock acquired.  This means its up to me to write a new cache file
 			$this->writeCacheFile($fp);
 			
-			fclose($fp);			// close the file
 			// rename used instead of copy for atomicity reasons
-			touch ($this->folder . $this->tmpCacheFile);	//touch before to set modify time
-			rename($this->folder . $this->tmpCacheFile, $this->folder . uniqid('cache'));
-			touch ($this->folder . $this->tmpCacheFile);	//touch after to create new tmp file
+			touch (dirname(__FILE__) . $this->folder . $this->tmpCacheFile);	//touch before to set modify time
+			$name = uniqid('c') . '.cache';
+			rename(dirname(__FILE__) . $this->folder . $this->tmpCacheFile, dirname(__FILE__) . $this->folder . $name);
+			touch (dirname(__FILE__) . $this->folder . $this->tmpCacheFile);	//touch after to create new tmp file
 			flock($fp, LOCK_UN);    // release the lock
+			fclose($fp);			// close the file
+			$this->removeOldCache();
 			
-			return;
+			echo "exclusive lock released: " . time() . "<br />";
+			return $name;
 		}
 		
 		public function forceExpiration() {
-			// Start by trying to obtain an exclusive lock on tmp file
-			$fp = fopen($this->folder . $this->tmpCacheFile, "r+");
 
-			if (!flock($fp, LOCK_EX | LOCK_NB)) {  // acquire an exclusive lock
-				// Someone else is already updating the cache file. Nothing more to do here.
-				fclose($fp);			
-				return $file;	//return old file.  Will return null if no suitable file exists.
-			} 
-
-			// Exclusive lock acquired.  This means its up to me to write a new cache file
-			$this->writeCacheFile($fp);
-			
-			fclose($fp);			// close the file
-			// rename used instead of copy for atomicity reasons
-			touch ($this->folder . $this->tmpCacheFile);	//touch before to set modify time
-			rename($this->folder . $this->tmpCacheFile, $this->folder . uniqid('cache'));
-			touch ($this->folder . $this->tmpCacheFile);	//touch after to create new tmp file
-			flock($fp, LOCK_UN);    // release the lock
-			
-			return;
 		}
 		
 		private function writeCacheFile($fp){
 			//this needs work
+			ftruncate($fp, 0);
+			date_default_timezone_set('UTC');
+			fwrite($fp, 'This file was started at: ' . date('Ydm-His'));
+			sleep(10);
+			fwrite($fp, 'This file was completed at: ' . date('Ydm-His'));
+			return;
 		}
 		
 		private function removeOldCache() {
 			//get the list of files in the cache folder.  There should only be cache files in here.
-			$files = getCacheFileList();
+			$files = $this->getCacheFileList();
 
 			// remove files from the oldest to newest until there's only a couple left
 			$rfiles = array_reverse($files, true);
-			for($rfiles as $key->$val){
-				if(count($rfiles <= 3) {
+			foreach($rfiles as $key=>$val){
+				if(count($rfiles) <= 3) {
 					break;
 				}
-				unset($files[$key]);
-				unlink($this->folder . $val);
+				unset($rfiles[$key]);
+				unlink(dirname(__FILE__) . $this->folder . $val);
 			}
 		
 		}
 		
 		private function retrieveCacheFile(){
 			//get the list of files in the cache folder.  There should only be cache files in here.
-			$files = getCacheFileList();
+			$files = $this->getCacheFileList();
 		   
 			//loop through files and return first one that isn't locked.
-			for($files as $modTime->$f) {
-				$fp = fopen($this->folder . $f, "r");
+			foreach($files as $modTime=>$f) {
+				$fp = fopen(dirname(__FILE__) . $this->folder . $f, "r");
 
 				if (flock($fp, LOCK_SH | LOCK_NB)) {  // acquire an reader lock
 					flock($fp, LOCK_UN);    // release the lock
@@ -110,21 +104,27 @@
 		private function getCacheFileList() {
 			//get the list of files in the cache folder.  There should only be cache files in here.
 			$files = array();
-			if ($handle = opendir($this->folder)) {
-			while (false !== ($file = readdir($handle))) {
-				if ($file != "." && $file != ".." && $file != $this->tmpCacheFile) {
-					$files[filemtime($file)] = $file;
+			if ($handle = opendir(dirname(__FILE__) . $this->folder)) {
+				while (false !== ($file = readdir($handle))) {
+					if ($file != "." && $file != ".." && $file != $this->tmpCacheFile && strpos($file, '.cache') !== false) {
+						$files[filemtime(dirname(__FILE__) . $this->folder . $file)] = $file;
+					}
 				}
+				closedir($handle);
 			}
-			closedir($handle);
 
 			ksort($files);		//should be a short number of files.
+			$rfiles = array_reverse($files, true);	//return the list with the newest files first
 		   
-			return $files;
+			return $rfiles;
 		}
 		
 		private function isExpired($file) {
-			return (time() - filemtime($file) >= $this->maxAge);		
+			$time = time();
+			$mtime = filemtime(dirname(__FILE__) . $this->folder . $file);
+			$diff =  $time - $mtime; 
+			
+			return ($diff >= $this->maxAge);		
 		}
 		
 	}
---
+++ example.php
@@ -0,0 +1,33 @@
+<?php 
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	require 'CacheManager.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE"> 
+		<title>
+			PHP Cache Manager Test
+		</title>
+	</head>
+	<body>
+		<div id="content">
+			<?php
+				//load this area with the contents of the cache file from the manager.
+				
+				$mgr = new CacheManager();
+				
+				for($ctr = 0; $ctr < 45; $ctr++) {
+					$f = $mgr->retrieveCache();
+					$filecontents = file_get_contents(dirname(__FILE__) . "/cache/" . $f);
+					echo "response received at: " . time() . "<br />";
+//					echo $ctr . " --- <br />" . $filecontents . "<br />";
+					sleep(1);
+				}
+			?>
+		</div>
+	</body>
+</html>
+
